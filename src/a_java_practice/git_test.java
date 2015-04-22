47283a6e4dc3b949171cb47cd3247df5f3bd3c31
vonbolt <guozhongxiao1990@gmail.com>
2015-04-22 21:04:09 +0900

    commit 2 2015-4-22 21:04:05

---
+++ src/a_java_practice/git_test.java
@@ -0,0 +1,7 @@
+package a_java_practice;
+
+public class git_test {
+
+}
+
+// commit 02 2015-4-22 21:03:30
--- src/a_java_practice/java_test.java
+++ src/a_java_practice/java_test.java
@@ -3,3 +3,5 @@ package a_java_practice;
 public class java_test {
 
 }
+
+// second commit 2015-4-22 21:00:57
